71cc51944733a53ceb5b720c1af64434bb363bc8
Simon Kusterer <simon@velite.de>
2012-03-04 13:36:23 +0100

    added _postProcess

--- source/jquery.dropit.js
+++ source/jquery.dropit.js
@@ -1,22 +1,20 @@
- /*
+/*
  * dropIt - jQuery Plugin
- * version: 1.0 (03/03/2012)
  *
  * Copyright 2012 Simon Kusterer - simon@soped.com
- *
  */
 
-(function($) {
-	jQuery.fn.dropIt = function(options) {
+(function ($) {
+	jQuery.fn.dropIt = function (options) {
 		var that = this;
 
 		that.options = $.extend({}, jQuery.fn.dropIt.defaults, options);
-		var _drop = function(evt) {
+		var _drop = function (evt) {
 			evt.originalEvent.stopPropagation();
 			evt.originalEvent.preventDefault();
 			var files = evt.originalEvent.dataTransfer.files;
 
-			for (var i=0; i<files.length; i++) {
+			for (var i = 0; i < files.length; i++) {
 				var file = files[i];
 				var type = _getFiletype(file.fileName);
 				if (type === false) {
@@ -24,19 +22,22 @@
 				}
 
 				var processor = that.options.fileTypes[type] || that.options.defaultProcessor;
-				jQuery.fn.dropIt.processors[processor].call(that, file, evt, _preProcess(file, evt));
+				var local = _preProcess(file, evt);
+				var content = jQuery.fn.dropIt.processors[processor].call(that, file, evt, local, function (content) {
+					_postProcess(content, evt, local);
+				});
 			}
 			return false;
 		};
 
-		var _dragOver = function(evt) {
+		var _dragOver = function (evt) {
 			evt.originalEvent.stopPropagation();
 			evt.originalEvent.preventDefault();
 			evt.originalEvent.dataTransfer.dropEffect = 'copy';
 			return false;
 		};
 
-		var _preProcess = function(file, evt) {
+		var _preProcess = function (file, evt) {
 			var insertMode = $(evt.srcElement).attr('data-dropit-insert-mode') || that.options.defaultInsertMode;
 			insertMode = insertMode.toLowerCase();
 			if (that.options.insertPrompt) {
@@ -46,68 +47,73 @@
 				insertMode = that.options.defaultInsertMode;
 			}
 			return {
-				'insertMode': insertMode
+				'insertMode':insertMode
 			}
 		};
 
-		var _getFiletype = function(fileName) {
+		var _postProcess = function (content, evt, local) {
+			$(evt.srcElement)[that.options.insertTypes[local.insertMode]](content);
+		};
+
+		var _getFiletype = function (fileName) {
 			fileName = fileName || '';
 			var pieces = fileName.split('.');
 			if (pieces.length === 0) {
 				return false;
 			}
-			return pieces[pieces.length-1].toLowerCase();
+			return pieces[pieces.length - 1].toLowerCase();
 		};
 
-		return this.each(function() {
+		return this.each(function () {
 			$(this).bind('dragover', _dragOver);
 			$(this).bind('drop', _drop);
 		});
 	};
 
 	jQuery.fn.dropIt.processors = {
-		'html': function(file, evt, local) {
+		'html':function (file, evt, local, fn) {
 			var that = this;
 			var reader = new FileReader();
-			reader.onload = (function(theFile) {
-				return function(e) {
-					$(evt.srcElement)[that.options.insertTypes[local.insertMode]](e.target.result);
+			reader.onload = (function (theFile) {
+				return function (e) {
+					fn.call(that, e.target.result);
 				};
 			})(file);
 			reader.readAsText(file);
 		},
-		'image': function(file, evt, local) {
-			var reader = new FileReader(),
-					imageTpl = this.options.tpl.image;
-			reader.onload = (function(theFile) {
-				return function(e) {
-					$(evt.srcElement)[local.insertMode](imageTpl.replace('{src}', e.target.result));
+		'image':function (file, evt, local, fn) {
+			var that = this;
+			var reader = new FileReader();
+			var imageTpl = this.options.tpl.image;
+			reader.onload = (function (f) {
+				return function (e) {
+					fn.call(that, imageTpl.replace('{src}', e.target.result));
 				};
 			})(file);
 			reader.readAsDataURL(file);
 		},
-		'unknown': function(file, evt, local) {
+		'unknown':function (file, evt, local, fn) {
 			//console.log('dunno what todo');
 		}
 	};
 
 	jQuery.fn.dropIt.defaults = {
-		'insertPrompt': false,
-		'tpl': {
-			'image': '<img src="{src}" />'
+		'insertPrompt':false,
+		'tpl':{
+			'image':'<img src="{src}" />'
 		},
-		'fileTypes': {
-			'html': 'html',
-			'jpg': 'image',
-			'png': 'image',
-			'gif': 'image'
+		'fileTypes':{
+			'html':'html',
+			'jpg':'image',
+			'png':'image',
+			'gif':'image'
 		},
-		'defaultInsertMode': 'append',
-		'insertTypes': {
-			'overwrite': 'html',
-			'append': 'append',
-			'prepend': 'prepend'
+		'defaultInsertMode':'append',
+		'insertTypes':{
+			'overwrite':'html',
+			'append':'append',
+			'prepend':'prepend'
 		},
-		defaultProcessor: 'unknown'
-	}
+		'defaultProcessor':'unknown'
+	};
 })(jQuery);
